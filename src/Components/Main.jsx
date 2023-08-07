cd5c1fc793f1c6031049c42917769f2fdbdbf680
Francisco Cornejo <00105018@uca.edu.sv>
2023-08-06 21:38:32 -0600

    Pokemon App

---
+++ src/Components/Main.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Card from "./Card";
+import Pokeinfo from "./Pokeinfo";
+
+const Main = () => {
+  return (
+    <>
+      <div className="container">
+        <div className="left-content">
+          <Card></Card>
+          <Card></Card>
+          <Card></Card>
+          <Card></Card>
+          <Card></Card>
+        </div>
+
+        <div className="right-content">
+            <Pokeinfo></Pokeinfo>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Main;
